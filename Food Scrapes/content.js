13a02a03b6a2a18c9e56b9577a982c579cd62a90
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2021-01-24 08:21:53 -0500

    Formatting
    Formatted files & changed background color / font size

--- Food Scrapes/content.js
+++ Food Scrapes/content.js
@@ -1,8 +1,3 @@
-// import JSSoup from 'jssoup'
-
-// alert('injected content')
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    // alert(message)
-
     sendResponse({'item': document.querySelector("#menuItem > fieldset > div:nth-child(2) > div > h1").innerText})
 })
--- Food Scrapes/popup.js
+++ Food Scrapes/popup.js
@@ -1,67 +1,67 @@
-const url = chrome.runtime.getURL('Menu/torchys_nutrition_information.json');
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').addEventListener('click',
-        onClick, false)
+const url = chrome.runtime.getURL("Menu/torchys_nutrition_information.json");
 const imgurl = chrome.runtime.getURL("Menu/torchys_logo.png");
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    document.querySelector("button").addEventListener("click", onClick, false);
 
     function onClick() {
-        chrome.tabs.query({currentWindow: true, active: true},
-        function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, 'bears test 123', setCount)
-        })
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, "", setCount);
+      });
     }
 
-    function setCount (res) {
-        fetch(url)
-            .then((response) => response.json())
-            
-            .then((json) => {
-                
-                const div2 = document.getElementById('button')
-                div2.style.display = 'none'
+    onClick();
 
-                const div = document.createElement('div')
-                menu_information = json[res.item.toString().toUpperCase()]
-                div.innerHTML +=
-                `
-                <div style="height:200px;width:450px;align-content:center; margin:0;background-color: #b80610;">
+    function setCount(res) {
+      fetch(url)
+        .then((response) => response.json())
+
+        .then((json) => {
+          const div2 = document.getElementById("button");
+          div2.style.display = "none";
+
+          const div = document.createElement("div"); // create pop-up display div
+          menu_information = json[res.item.toString().toUpperCase()]; // get corresponding menu item
+          // append to pop-up
+          div.innerHTML = `
+                <div style="height:200px;width:450px;align-content:center; margin:0;background-color: #e6434c;">
                     <div style=  "position:absolute; align-content:center "> 
-                        <img style="object-fit: scale-down;height:auto; max-width:95%; margin: 10px; " src=${imgurl}/>
+                        <img style="object-fit: scale-down;height:auto; max-width:95%; margin: 10px; " src="${imgurl}"/>
                     </div>
                     <div style="height:85px; width:450px; padding-top:65px; display:flex; justify-content:center; align-items:center; position:relative; z-index:10;">
-                        <div style="text-align:center; font-size: 40px"; font-family:Copperplate> ${menu_information['Calories']} Calories</div> 
+                        <div style="text-align:center; font-size: 40px"; font-family:Copperplate> ${menu_information["Calories"]} Calories</div> 
                     </div>
                     <div style= "display:flex; z-index:10; align-content:center; justify-content:space-between; margin-left:20px; margin-right:20px; position:relative;">
                         <div style="align-content:center;">
-                            <div style="text-align:center; font-size:18px">Protein</div>
-                            <div style="text-align:center; ">${menu_information['Protein']} g</div>
+                            <div style="text-align:center; font-size:20px">Protein</div>
+                            <div style="text-align:center; font-size:16px">${menu_information["Protein"]} g</div>
                         </div>
 
                         <div style="align-content:center;">
-                            <div style="text-align:center; font-size:18px;">Total Carbs</div>
-                            <div style="text-align:center;">${menu_information['Total Carbs']} g</div>
+                            <div style="text-align:center; font-size:20px;">Total Carbs</div>
+                            <div style="text-align:center; font-size:16px">${menu_information["Total Carbs"]} g</div>
                         </div>
 
                         <div style="align-content:center;">
-                            <div style="text-align:center;font-size:18px;">Total Fat</div>
-                            <div style="text-align:center;">${menu_information['Total Fat']} g</div>
+                            <div style="text-align:center; font-size:20px;">Total Fat</div>
+                            <div style="text-align:center; font-size:16px">${menu_information["Total Fat"]} g</div>
                         </div>
 
                         <div style="align-content:center;">
-                            <div style="text-align:center;font-size:18px;">Sodium</div>
-                            <div style="text-align:center;">${menu_information['Sodium']} mg</div>
+                            <div style="text-align:center; font-size:20px;">Sodium</div>
+                            <div style="text-align:center; font-size:16px">${menu_information["Sodium"]} mg</div>
                         </div>
                     </div>
 
                 </div>
-                ` 
+            `;
 
-                document.body.appendChild(div)
-            });
-        
-        
+          document.body.appendChild(div);
+        });
     }
+  },
+  false
+);
 
-}, false)
-
-StyleSheet
+StyleSheet;
