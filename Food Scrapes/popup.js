bd0febfcef53becec4f8aa7d91d39ef320cd7897
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2021-01-24 09:26:21 -0500

    Remove button
    Changed code to start the query when the extension is triggered rather than when the button is toggled.

--- Food Scrapes/popup.js
+++ Food Scrapes/popup.js
@@ -3,15 +3,9 @@ const imgurl = chrome.runtime.getURL("Menu/torchys_logo.png");
 document.addEventListener(
   "DOMContentLoaded",
   function () {
-    document.querySelector("button").addEventListener("click", onClick, false);
-
-    function onClick() {
-      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, "", setCount);
-      });
-    }
-
-    // onClick();
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, "", setCount);
+    });
 
     function setCount(res) {
       const div = document.createElement("div");
